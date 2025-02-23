7e95c4c3dea06915788c6cc9062a976ac1473ace
StudentAndreplus <mcdonala9086@student.faytechcc.edu>
2025-02-22 22:59:38 -0500

    Add files via upload

---
+++ P1LAB_McDonaldAndre.py
@@ -0,0 +1,8 @@
+# Andre McDonald
+# 02/22/25
+# P1LAB1
+# Writing a program to take student first and last name and welcome to course
+
+first_name = input()
+last_name = input()
+print ("Hello there!", first_name , last_name , "welcome to CTI-110" )
